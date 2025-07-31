605ebf4f3237ac19ab2fe9686f75a6da4097188d
pika3113 <boomthering@gmail.com>
2025-07-31 17:14:41 +0800

    ~

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,7 +1,7 @@
 
 // development
-const API_BASE_URL = 'http://localhost:8000';
+// const API_BASE_URL = 'http://localhost:8000';
 
 // production
-// const API_BASE_URL = 'https://backend-production-8e98.up.railway.app'
+const API_BASE_URL = 'https://backend-production-8e98.up.railway.app'
 export { API_BASE_URL };
