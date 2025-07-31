6624aec2714c0f9fbb06fa67c5373396366f8184
pika3113 <boomthering@gmail.com>
2025-07-31 12:31:14 +0800

    removed trailing '/

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,5 +3,5 @@
 // const API_BASE_URL = 'http://localhost:8000';
 
 // production
-const API_BASE_URL = 'https://backend-production-8e98.up.railway.app/'
+const API_BASE_URL = 'https://backend-production-8e98.up.railway.app'
 export { API_BASE_URL };
